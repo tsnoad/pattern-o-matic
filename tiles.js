ef77958f1fbe3a87c1d47ec4817524e08b74ab08
tsnoad <tsnoad@gmail.com>
2012-06-11 00:48:19 +1000

    add more files

---
+++ tiles.js
@@ -0,0 +1,341 @@
+//tile 1: black squares
+tile1 = {
+	hue: 0,
+	saturation: 0,
+	lightness: 12,
+	highlight: 3,
+	noise: 7,
+	patternsize: 4,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, false, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 2: white medium arrows
+tile2 = {
+	hue: 264,
+	saturation: 3,
+	lightness: 83,
+	highlight: 4,
+	noise: 10,
+	patternsize: 5,
+	pattern: [
+		false, true, true, true, true, false, false,
+		false, false, true, true, true, false, false,
+		false, false, false, true, true, false, false,
+		false, false, false, false, true, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 3: green vertical stripes
+tile3 = {
+	hue: 102,
+	saturation: 38,
+	lightness: 29,
+	highlight: 3,
+	noise: 8,
+	patternsize: 4,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 4: blue squares
+tile4 = {
+	hue: 213,
+	saturation: 34,
+	lightness: 18,
+	highlight: 4,
+	noise: 9,
+	patternsize: 4,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 5: bright green diagonal lines
+tile5 = {
+	hue: 96,
+	saturation: 85,
+	lightness: 22,
+	highlight: 4,
+	noise: 8,
+	patternsize: 4,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, true, false, true, false, false, false,
+		true, false, true, true, false, false, false,
+		false, true, true, true, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 6: blue/gray cross hatches
+tile6 = {
+	hue: 219,
+	saturation: 30,
+	lightness: 23,
+	highlight: 6,
+	noise: 8,
+	patternsize: 4,
+	pattern: [
+		false, true, false, false, false, false, false,
+		false, false, false, true, false, false, false,
+		true, false, false, false, false, false, false,
+		false, false, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 7: pale blue vertical stripes, alternating thickness
+tile7 = {
+	hue: 207,
+	saturation: 66,
+	lightness: 73,
+	highlight: 12,
+	noise: 12,
+	patternsize: 5,
+	pattern: [
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 8: olive negative diagonal interrupted lines
+tile8 = {
+	hue: 69,
+	saturation: 35,
+	lightness: 26,
+	highlight: 5,
+	noise: 9,
+	patternsize: 3,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, true, false, false, false, false, false,
+		true, false, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 9: dark gray houndstooth-like pattern
+tile9 = {
+	hue: 0,
+	saturation: 0,
+	lightness: 14,
+	highlight: 4,
+	noise: 8,
+	patternsize: 3,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, false, false, false, false, false, false,
+		false, true, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 10: pale purple squares
+tile10 = {
+	hue: 264,
+	saturation: 22,
+	lightness: 41,
+	highlight: 4,
+	noise: 8,
+	patternsize: 4,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		true, true, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 11: dark purple diagonal stripes
+tile11 = {
+	hue: 294,
+	saturation: 28,
+	lightness: 11,
+	highlight: 4,
+	noise: 6,
+	patternsize: 5,
+	pattern: [
+		false, true, true, false, false, false, false,
+		true, true, false, false, false, false, false,
+		true, false, false, false, true, false, false,
+		false, false, false, true, true, false, false,
+		false, false, true, true, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 12: gray medium arrows
+tile12 = {
+	hue: 264,
+	saturation: 3,
+	lightness: 21,
+	highlight: 4,
+	noise: 6,
+	patternsize: 5,
+	pattern: [
+		false, true, true, true, true, false, false,
+		false, false, true, true, true, false, false,
+		false, false, false, true, true, false, false,
+		false, false, false, false, true, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 13: light gray small arrows
+tile13 = {
+	hue: 0,
+	saturation: 0,
+	lightness: 79,
+	highlight: 7,
+	noise: 6,
+	patternsize: 3,
+	pattern: [
+		false, true, true, false, false, false, false,
+		false, false, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile14: dark gray diagonal stripes
+tile14 = {
+	hue: 0,
+	saturation: 0,
+	lightness: 11,
+	highlight: 2,
+	noise: 3,
+	patternsize: 4,
+	pattern: [
+		true, true, true, false, false, false, false,
+		true, true, false, true, false, false, false,
+		true, false, true, true, false, false, false,
+		false, true, true, true, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 15: small blue checks
+tile15 = {
+	hue: 219,
+	saturation: 30,
+	lightness: 23,
+	highlight: 6,
+	noise: 9,
+	patternsize: 4,
+	pattern: [
+		false, true, true, false, false, false, false,
+		true, false, false, true, false, false, false,
+		true, false, false, true, false, false, false,
+		false, true, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile16: blue diagonal stripes, alternating thickness
+tile16 = {
+	hue: 207,
+	saturation: 50,
+	lightness: 25,
+	highlight: 4,
+	noise: 7,
+	patternsize: 7,
+	pattern: [
+		false, true, true, false, false, true, false,
+		true, true, false, false, true, false, false,
+		true, false, false, true, false, false, true,
+		false, false, true, false, false, true, true,
+		false, true, false, false, true, true, false,
+		true, false, false, true, true, false, false,
+		false, false, true, true, false, false, true
+	],
+};
+
+//tile 17: pale blue vertical stripes, alternating thickness
+tile17 = {
+	hue: 48,
+	saturation: 28,
+	lightness: 25,
+	highlight: 4,
+	noise: 8,
+	patternsize: 5,
+	pattern: [
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		true, true, false, true, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
+
+//tile 18: blue small arrows
+tile18 = {
+	hue: 207,
+	saturation: 50,
+	lightness: 25,
+	highlight: 4,
+	noise: 6,
+	patternsize: 3,
+	pattern: [
+		false, true, true, false, false, false, false,
+		false, false, true, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false,
+		false, false, false, false, false, false, false
+	],
+};
